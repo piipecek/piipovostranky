62c760fd8920289bb43f35a1dcaf4481158810cf
Josef Lát <josef.latj@gmail.com>
2023-10-11 13:51:19 +0200

    zakladni jazyky

--- website/models/language.py
+++ website/models/language.py
@@ -6,9 +6,14 @@ class Language(Common_methods_db_model):
     system_name = db.Column(db.String(200))
     display_name = db.Column(db.String(200))
     translations = db.relationship("Translation", backref="language")
+    
     def __repr__(self) -> str:
         return f"Jazyk | {self.system_name}"
     
+    @staticmethod
+    def get_by_system_name(name) -> "Language":
+        return db.session.scalars(db.select(Language).where(Language.system_name == name)).first()
+        
     def get_detail_for_seznam_jazyku(self) -> dict:
         return {
             "id": self.id,
