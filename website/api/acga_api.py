f08262b5a7987910875d64de3bc2d68fc717fc22
Josef Lát <josef.latj@gmail.com>
2024-02-27 21:22:53 +0100

    souhrne stats evaluace

--- website/api/acga_api.py
+++ website/api/acga_api.py
@@ -37,12 +37,6 @@ def evaluace_locked(uuid):
     else:
         return None
 
-@acga_api.route("/evaluace_statistiky_data", methods=["POST"])
-@require_role_system_name_on_current_user("acga_ucitel")
-def evaluace_statistiky_data():
-    date = datetime.fromisoformat(request.form.get("date"))
-    print(date)
-    return request.form.to_dict()
  
 @acga_api.route("/vytvorit_evaluace/<int:pocet>", methods=["GET","POST"])
 @require_role_system_name_on_current_user("user")
@@ -50,9 +44,71 @@ def vytvorit_evlauace(pocet):
     result = Evaluace.vytvorit_evaluace(pocet)
     return result
 
+
 @acga_api.route("/vazeny_prumer", methods=["POST"])
 def vazeny_prumer():
     file = request.files["file"]
     data  = request.form.to_dict()
     result = pocitani_prumeru(file=file, data=data)
     return json.dumps(result, indent=3)
+
+
+@acga_api.route("/evaluace_statistiky_data", methods=["POST"])
+@require_role_system_name_on_current_user("acga_ucitel")
+def evaluace_statistiky_data():
+    date = datetime.fromisoformat(request.form.get("date"))
+    result = []
+    # pripustna evaluace do shrnuti je 1) od spravnyho ucitele 2) odevzdana 3) novejsi nez dane datum
+    pripustne_evaluace = [e for e in current_user.evaluace if e.datetime_vytvoreni > date and e.je_odevzdana]
+    for e in pripustne_evaluace:
+        e: Evaluace
+        data_evaluace = json.loads(e.data_json)
+        for otazka in data_evaluace:
+            if otazka["id"] not in [entry["id"] for entry in result]: # pokud tohle ID ještě nemá histogramovou kategorii, zalozim ji
+                # budu mit dva typy: histogram a otevrena. Do histogramu patri single, multiple i ciselna.
+                if otazka["typ"] == "ciselna":
+                    popisky = [str(i) for i in range(1, otazka["max"]+1)]
+                    popisky[0] += ": Zcela souhlasím"
+                    popisky[-1] += ": Vůbec neuhlasím"
+                    typ = "histogram"
+                elif otazka["typ"] == "single":
+                    popisky = otazka["choices"]
+                    typ = "histogram"
+                elif otazka["typ"] == "multiple":
+                    popisky = otazka["choices"]
+                    typ = "histogram"
+                else:
+                    popisky = None
+                    typ = "otevrena"
+                    
+                novy_zaznam = {
+                        "id": otazka["id"],
+                        "otazka": otazka["otazka"],
+                        "typ": typ,
+                    }    
+                if typ == "histogram":
+                    novy_zaznam["x"] = popisky
+                    novy_zaznam["y"] = [0 for _ in popisky]
+                else:
+                    novy_zaznam["odpovedi"] = []
+                
+                result.append(novy_zaznam)
+            # teď už záznam buď existoval předtim, nebo byl nově vytvořenej
+            #najdu ten existujici zaaznam 
+            zaznam = None
+            for z in result:
+                if z["id"] == otazka["id"]:
+                    zaznam = z
+                    break
+            # a zapocitam.
+            
+            if otazka["typ"] == "ciselna" and otazka["value"]:
+                zaznam["y"][otazka["value"]-1] += 1
+            elif otazka["typ"] == "single" and otazka["value"] is not None:
+                zaznam["y"][otazka["value"]] += 1
+            elif otazka["typ"] == "multiple" and otazka["values"]:
+                for value in otazka["values"]:
+                    zaznam["y"][value] += 1
+            elif otazka["typ"] == "otevrena":
+                zaznam["odpovedi"].append(otazka["value"])
+    return json.dumps(result, indent=4)
--- website/static/js/acga/evaluace_statistiky.js
+++ website/static/js/acga/evaluace_statistiky.js
@@ -1,4 +1,5 @@
 let ukazat_button = document.getElementById("ukazat")
+let result_div = document.getElementById("result")
 
 ukazat_button.addEventListener("click", function() {
     document.getElementById("form").hidden = true
@@ -11,7 +12,7 @@ ukazat_button.addEventListener("click", function() {
         contentType: false,
         processData: false,
         success: function(data) {
-            console.log(data, "yayyyyy")
+            handle_response(data)
         }
     })
 })
@@ -21,5 +22,72 @@ let ago = new Date()
 ago.setMonth(today.getMonth() - 2)
 let formattedDate = ago.toISOString().split('T')[0];
 document.getElementById("date").value = formattedDate
+
+function handle_response(response) {
+    response = JSON.parse(response)
+    result_div.hidden = false
+    for (let zaznam of response) {
+        let wrapper = document.createElement("div")
+        let inner_div = document.createElement("div")
+        inner_div.classList.add("plot-div")
+        wrapper.classList.add("plot-div-wrapper")
+        result_div.appendChild(wrapper)
+        wrapper.appendChild(inner_div)
+        let p = document.createElement("p")
+        p.classList.add("otazka")
+        p.innerText = zaznam.otazka
+        inner_div.appendChild(p)
+        if (zaznam.typ == "histogram") {
+            let canvas_div_wrapper = document.createElement("div")
+            canvas_div_wrapper.classList.add("canvas-div-wrapper")
+            let canvas_div = document.createElement("div")
+            canvas_div.classList.add("canvas-div")
+            inner_div.appendChild(canvas_div_wrapper)
+            canvas_div_wrapper.appendChild(canvas_div)
+            let canvas = document.createElement("canvas")
+            canvas_div.appendChild(canvas)
+            var ctx = canvas.getContext('2d');
+        
+            var myChart = new Chart(ctx, {
+                type: 'bar', // Change to horizontalBar
+                data: {
+                    labels: zaznam.x,
+                    datasets: [{
+                        label: 'počet odpovědí',
+                        data: zaznam.y,
+                        backgroundColor: 'rgba(136, 200, 85, 0.4)',
+                        borderColor: 'rgba(136, 200, 85, 1)',
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true,
+                            ticks: {
+                                stepSize: 1
+                            }
+                        }
+                    },
+                    plugins: {
+                        legend: {
+                            display: false
+                        }
+                    }
+                }
+            });
+        } else if (zaznam.typ == "otevrena") {
+            for (let odpoved of zaznam.odpovedi) {
+                let odpoved_div = document.createElement("div")
+                odpoved_div.classList.add("odpoved-div")
+                if (odpoved.trim() != "") {
+                    odpoved_div.innerText = odpoved
+                    console.log(odpoved)
+                    inner_div.appendChild(odpoved_div)
+                }
+            }
+        }
+    }
+}
 
 
