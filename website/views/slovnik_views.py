b133257fcd71af51ae93a1cbf33a0c0ebf8999b2
Josef Lát <josef.latj@gmail.com>
2026-02-19 23:02:05 +0100

    nove endpointy a stranky na slovnik

--- website/views/slovnik_views.py
+++ website/views/slovnik_views.py
@@ -7,23 +7,37 @@ slovnik_views = Blueprint("slovnik_views",__name__)
 @slovnik_views.route("/")
 @require_role_system_name_on_current_user("user")
 def slovnik_home():
+    return render_template("slovnik/dashboard.html", roles=get_roles())
+
+
+@slovnik_views.route("/docs")
+@require_role_system_name_on_current_user("user")
+def docs():
+    return render_template("slovnik/docs.html", roles=get_roles())
+    
+    
+@slovnik_views.route("/terms", methods=["GET", "POST"])
+@require_role_system_name_on_current_user("user")
+def terms():
     if request.method == "GET":
-        return render_template("slovnik/dashboard.html", roles=get_roles())
+        return render_template("slovnik/terms.html", roles=get_roles())
     else:
         return request.form.to_dict()
     
-@slovnik_views.route("/slovnik")
-@require_role_system_name_on_current_user("user")
-def slovnik():
-    if request.method == "GET":
-        return render_template("slovnik/slovnik.html", roles=get_roles())
-    else:
-        return request.form.to_dict()
     
-@slovnik_views.route("/nova_slovicka")
+@slovnik_views.route("/decks", methods=["GET", "POST"])
 @require_role_system_name_on_current_user("user")
-def nova_slovicka():
+def decks():
     if request.method == "GET":
-        return render_template("slovnik/nova_slovicka.html", roles=get_roles())
+        return render_template("slovnik/decks.html", roles=get_roles())
+    else:
+        return request.form.to_dict()    
+    
+    
+@slovnik_views.route("/new_terms", methods=["GET", "POST"])
+@require_role_system_name_on_current_user("user")
+def new_terms():
+    if request.method == "GET":
+        return render_template("slovnik/new_terms.html", roles=get_roles())
     else:
         return request.form.to_dict()
